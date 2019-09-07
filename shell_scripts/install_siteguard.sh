3118a8d8c8bcc3e9d0a36295c65bf364228e9da7
gouf <innocent.zero@gmail.com>
2019-09-07 21:20:50 +0900

    Extract some values to variables

--- shell_scripts/install_siteguard.sh
+++ shell_scripts/install_siteguard.sh
@@ -2,9 +2,16 @@
 
 WORK_DIR="$(pwd)"
 
+# SiteGuard instruction params
+ARCH="x86_64"
+WEB_SERVER="nginx"
+SITEGUARD_SITE_VERSION="4.0.0"
+SITEGUARD_FILE_VERSION="4.00-2"
+SITEGUARD="siteguard-server-edition-$SITEGUARD_FILE_VERSION.$WEB_SERVER.$ARCH"
+
 cd \
-&& wget http://progeny.sakura.ad.jp/siteguard/4.0.0/nginx/siteguard-server-edition-4.00-2.nginx.x86_64.tar.gz \
-&& tar xzf siteguard-server-edition-4.00-2.nginx.x86_64.tar.gz \
-&& cd siteguard-server-edition-4.00-2.nginx.x86_64 \
+&& wget "http://progeny.sakura.ad.jp/siteguard/$SITEGUARD_SITE_VERSION/$WEB_SERVER/$SITEGUARD.tar.gz" \
+&& tar xzf "$SITEGUARD.tar.gz" \
+&& cd "$SITEGUARD" \
 && make install \
-&& cd $WORK_DIR
+&& cd "$WORK_DIR"
